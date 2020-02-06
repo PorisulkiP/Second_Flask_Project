c6cc11640b8915d0dd8080be04e2cc699f6a85ad
PorisulkiP <alex1228@gmail.com>
2020-02-06 19:19:48 +0300

    2

--- models.py
+++ models.py
@@ -12,13 +12,36 @@ class Abstract(Base):
     id = Column(Integer, primary_key=True)
     created_on = Column(DateTime, default=datetime.datetime.now())
 
-class User(Base, Abstract):
+class User(Abstract, Base):
     __tablename__= 'users'
-    name = Column(String(30), unque=True, nullable=False)
-    name = Column(String(40), unque=True, nullable=False)
+    name = Column(String(30), unique=True, nullable=False)
+    email = Column(String(40), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
 
     user_links = relationship('Link')
 
     def __str__(self):
-        return '  |  '.join(self.name, self.email, self.password)
+        return '  |  '.join([self.id, self.name, self.email, self.password])
+
+class Task(Abstract, Base):
+    __tablename__= 'tasks'
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    title = Column(String(100), unique=True, nullable=False)
+    content = Column(String(500), nullable=False)
+    url = Column(Text, unique=True, nullable=False)
+    rating = Column(Integer, default=0)
+    tags = Column(Text, nullable=False)
+
+    author = relationship('User')
+
+    def __str__(self):
+        return '  |  '.join([self.id, self.user_id, self.title, self.url])
+
+Base.metadata.create_all()
+
+    
+
+
+
+
+
